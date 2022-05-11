8f95e19527c5f99303c0b748c08ea4c74e5924c7
chenyaling175 <82302511+chenyaling175@users.noreply.github.com>
2022-05-11 13:26:26 +0800

    Update HelloWorld.java

--- base/src/day01/HelloWorld.java
+++ base/src/day01/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("HelloWorld!");
         System.out.println("HelloGit!");
         System.out.println("HelloChen!");
+        System.out.println("Hello!");
     }
 }
